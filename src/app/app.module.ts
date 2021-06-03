1ff7c4481a037720441fccdd60c2d6e3e2a04a3b
CORP\thatle <tejas-milind.hatle@capgemini.com>
2021-06-03 18:38:11 +0530

    added services, added routes and added guarding against route activation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,36 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { ToastrService } from './common/toastr.service';
+import { RouterModule } from "@angular/router";
 import { EventsAppComponent } from './event-app.component';
+import { EventDetailsComponent } from './events/event-details/event-details.component';
 import { EventsListComponent } from './events/events-list.component';
 import { EventThumbnailComponent } from './events/events-thumbnail.component';
+import { EventService } from './events/shared/event.service';
 import { NavBarComponent } from './nav/navbar.component';
+import { appRoutes } from './routes';
+import { CreateEventComponent } from './events/create-event.component';
+import { Error404Component } from './errors/404.component';
+import { EventRouteActivator } from './events/event-details/event-route-activator.service';
 
 @NgModule({
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   declarations: [
     EventsAppComponent,
     EventsListComponent,
     EventThumbnailComponent,
-    NavBarComponent
+    NavBarComponent,
+    EventDetailsComponent,
+    CreateEventComponent,
+    Error404Component
+  ],
+  providers: [
+    EventService,
+    ToastrService,
+    EventRouteActivator
   ],
   bootstrap: [EventsAppComponent]
 })
