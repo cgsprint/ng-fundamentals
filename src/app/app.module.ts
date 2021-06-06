cdc6bfabca55e5d8dcbfb659e8a36e68773fc103
CORP\thatle <tejas-milind.hatle@capgemini.com>
2021-06-06 14:41:49 +0530

    creating modal component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { Component, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { TOASTER_TOKEN, Toastr } from './common/toastr.service';
+import { JQ_TOKEN, TOASTER_TOKEN, Toastr, CollapsibleWellComponent, SimpleModalComponent } from "./common/index";
 import { RouterModule } from "@angular/router";
 
 import { 
@@ -16,16 +16,19 @@ import {
   DurationPipe
 } from "./events/index";
 
+
+
 import { EventsAppComponent } from './event-app.component';
 import { NavBarComponent } from './nav/navbar.component';
 import { appRoutes } from './routes';
 import { Error404Component } from './errors/404.component';
 import { AuthService } from './user/auth.service';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { CollapsibleWellComponent } from './common/collapsible-well.component';
+// import { CollapsibleWellComponent } from './common/collapsible-well.component';
 
 // declare let toastr:Toastr
 let toastr:Toastr = window['toastr'];
+let jQuery:Toastr = window['$'];
 
 @NgModule({
   imports: [
@@ -45,11 +48,13 @@ let toastr:Toastr = window['toastr'];
     CreateSessionComponent,
     SessionListComponent,
     CollapsibleWellComponent,
-    DurationPipe
+    DurationPipe,
+    SimpleModalComponent
   ],
   providers: [
     EventService,
     { provide: TOASTER_TOKEN, useValue: toastr},
+    { provide: JQ_TOKEN, useValue: jQuery},
     EventRouteActivator,
     EventListResolver,
     AuthService,
