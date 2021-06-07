3c6c788d7f84d6a3f2ff0a2c010310b0cf77be15
CORP\thatle <tejas-milind.hatle@capgemini.com>
2021-06-07 21:11:49 +0530

    Integrating authentication with the server

--- src/app/user/login.component.ts
+++ src/app/user/login.component.ts
@@ -13,6 +13,7 @@ export class LoginComponent{
     userName
     password
     mouseoverLogin
+    loginInvalid= false;
     constructor(private authServie:AuthService, private router:Router){
 
     }
@@ -20,7 +21,15 @@ export class LoginComponent{
     login(formValues)
     {
         this.authServie.loginUser(formValues.userName, formValues.password)
-        this.router.navigate(['events'])
+            .subscribe(resp => {
+                if(!resp){
+                    this.loginInvalid = true;
+                }
+                else{
+                    this.router.navigate(['events'])
+                }
+            })
+        // this.router.navigate(['events'])
     }
 
     cancel(){
